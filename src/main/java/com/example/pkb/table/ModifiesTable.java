6fcc7c9cc48b4972f46f02e63d87d990701dc63d
PawelKrolDS360 <pawel.krol@ds360.pl>
2025-05-23 19:21:03 +0200

    Added maps for other types of statements that can modify variables

--- src/main/java/com/example/pkb/table/ModifiesTable.java
+++ src/main/java/com/example/pkb/table/ModifiesTable.java
@@ -1,16 +1,21 @@
 package com.example.pkb.table;
 
+import com.example.frontend.WrapperStatement;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
 import java.util.Set;
+import java.util.Stack;
 import java.util.TreeSet;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ModifiesTable {
     private static ModifiesTable instance;
     private final HashMap<String, Set<Integer>> variableModifiedByAssign = new HashMap<>();
+    private final HashMap<String, Set<Integer>> variableModifiedByWhile = new HashMap<>();
+    private final HashMap<String, Set<Integer>> variableModifiedByIf = new HashMap<>();
+    private final HashMap<String, Set<String>> variableModifiedByProcedure = new HashMap<>();
 
     public static ModifiesTable getInstance() {
         if (instance == null) {
@@ -19,12 +24,42 @@ public class ModifiesTable {
         return instance;
     }
 
-    public void addModifies(int lineNumber, String variableName) {
-        Set<Integer> modifies = variableModifiedByAssign.computeIfAbsent(variableName, k -> new TreeSet<>());
-        modifies.add(lineNumber);
+    public void addModifies(int lineNumber, String variableName, Stack<WrapperStatement> stack) {
+        Set<Integer> assignModifies = variableModifiedByAssign.computeIfAbsent(variableName, k -> new TreeSet<>());
+        assignModifies.add(lineNumber);
+
+        stack.forEach(wrapperStatement -> {
+            switch (wrapperStatement.type()) {
+                case PROCEDURE -> {
+                    Set<String> modifies = variableModifiedByProcedure.computeIfAbsent(variableName, k -> new TreeSet<>());
+                    modifies.add(wrapperStatement.procedureName());
+                }
+                case IF -> {
+                    Set<Integer> modifies = variableModifiedByIf.computeIfAbsent(variableName, k -> new TreeSet<>());
+                    modifies.add(wrapperStatement.codeLine());
+                }
+                case WHILE -> {
+                    Set<Integer> modifies = variableModifiedByWhile.computeIfAbsent(variableName, k -> new TreeSet<>());
+                    modifies.add(wrapperStatement.codeLine());
+                }
+                default -> throw new IllegalStateException("Achievement get! How did we get there?");
+            }
+        });
     }
 
     public Set<Integer> getAssignsModifyingVariable(String variableName) {
         return variableModifiedByAssign.getOrDefault(variableName, Set.of());
+    }
+
+    public Set<String> getProceduresModifyingVariable(String variableName) {
+        return variableModifiedByProcedure.getOrDefault(variableName, Set.of());
+    }
+
+    public Set<Integer> getWhilesModifyingVariable(String variableName) {
+        return variableModifiedByWhile.getOrDefault(variableName, Set.of());
+    }
+
+    public Set<Integer> getIfsModifyingVariable(String variableName) {
+        return variableModifiedByIf.getOrDefault(variableName, Set.of());
     }
 }
