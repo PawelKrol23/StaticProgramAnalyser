b2289419f300d1629ad05b8a9f20c57a14764c67
Arkadiusz Mroczkowski <80561@student.pb.edu.pl>
2025-06-14 22:33:24 +0200

    Added PKB Follows

---
+++ src/main/java/com/example/pkb/table/FollowsTable.java
@@ -0,0 +1,48 @@
+package com.example.pkb.table;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.*;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class FollowsTable {
+    private static FollowsTable instance;
+
+    // Follows: stmt1 -> stmt2
+    private final Map<Integer, Integer> followsMap = new HashMap<>();
+
+    public static FollowsTable getInstance() {
+        if (instance == null) instance = new FollowsTable();
+        return instance;
+    }
+
+    public void addFollows(int stmt1, int stmt2) {
+        followsMap.put(stmt1, stmt2);
+        System.out.println("FOLLOWS: " + stmt1 + " -> " + stmt2);
+    }
+
+    public boolean isFollows(int stmt1, int stmt2) {
+        return followsMap.getOrDefault(stmt1, -1) == stmt2;
+    }
+
+    // 🆕 potrzebne w PQL getCondition:
+    public Integer getFollows(int stmt1) {
+        return followsMap.get(stmt1);
+    }
+
+    public Integer getFollowedBy(int stmt2) {
+        for (Map.Entry<Integer, Integer> entry : followsMap.entrySet()) {
+            if (entry.getValue() == stmt2) return entry.getKey();
+        }
+        return null;
+    }
+
+    public Set<Integer> getAllFollowKeys() {
+        return followsMap.keySet();
+    }
+
+    public Set<Integer> getAllFollowValues() {
+        return new HashSet<>(followsMap.values());
+    }
+}
